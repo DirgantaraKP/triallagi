c2f8868ab0533c5447e59410e1d8e492e48b6678
DirgantaraKP <dirgantaraputra14@student.uns.ac.id>
2020-05-27 21:09:51 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git</title>
+</head>
+<body>
+    <h1>apapunn</h1>
+    <p>apapun</p>
+</body>
+</html>
